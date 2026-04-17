64ddf8203c8c9246bd601fd4a6bfa10a2ef38b8d
CamiSteinn <camilaehlertm@gmail.com>
2026-04-17 11:32:14 -0300

    inicia est

---
+++ src/Entrega_2/EstimativaEstatistica.py
@@ -0,0 +1,97 @@
+import numpy as np
+
+class Equacioanmento_PW():
+    
+    def __init__(self):
+        self.aircraft = {'W/S': [],
+                         'P/W': []}
+        
+
+        # --- Performance
+        self.W0_lb = 1172.2
+
+        lb_to_kg = 0.453592
+
+        self.W0_kg = self.W0_lb*lb_to_kg
+
+        self.eta_prop = 0.8 # propeller efficiency
+
+        # --- Missions
+        kmph_to_mps = 1/3.6
+
+        kmph_to_ftps = 0.911344
+
+        self.rho_kgpm3_SL = 1.225 # at Sea Level
+
+        self.V_cruise_mps = 250*kmph_to_mps
+        self.V_cruise_fts = 250*kmph_to_ftps
+        self.Vmax_mps = 300*kmph_to_mps
+        self.Vmax_fts = 300*kmph_to_ftps
+
+        self.RoC_mps = 17 # missão 1, reconhecimento
+        
+        # --- Geometry
+        self.AR = 14
+
+        # --- Aerodynamics
+        self.CLmax = 1.8
+
+        self.e = 0.9 # wing efficiency
+        
+        self.CD0 = 0.03
+        self.k = 1/(np.pi*self.AR*self.e)
+        self.CD = self.CD0 + self.k*(self.CLmax**2)
+
+        self.LDmax = 18
+        self.LDcruise = self.LDmax
+        self.LDloiter = self.LDmax*0.86
+        self.LDmax_climb = self.LDmax # poderia assumir um valor de proporcionalidade
+        
+
+        self.V_stall_mps = np.sqrt( (2*self.W0_kg)/(2*self.rho_kgpm3_SL*self.CLmax))
+
+    def P_W_vmax(self):
+        a = 0.025
+        C = 0.22
+
+        P_W = (a*self.Vmax_fts**C)
+
+        return P_W
+    
+    def T_Wcruise(self):
+
+        T_W = 1/self.LDcruise
+
+        return T_W
+    
+    def conversionP_W(self, T_W):
+        P_W = T_W/(self.eta_prop*550/self.V_cruise_fts)
+
+        return P_W
+    
+    def conversionCruiseTakeoff(self,P_Wcruise):
+        # Pelo gráfico 5.2 do Sadraey,escolhendo a curva 0-320:
+    
+    #para h_takeoff = 1000m --> P = 150 hp
+    #para h_serviço  = 3000m --> P = 110 hp
+        Pto_Pc = 150/110
+        W_takeoff = self.W0_lb
+        W_cruise1 = 1120
+ 
+        P_Wtakeoff = P_Wcruise*(W_cruise1/self.W0_lb)*(150/110)
+
+        return P_Wtakeoff
+    
+aircraft = Equacioanmento_PW()
+
+#estimativa velocidade máxima:
+P_W_vmax = aircraft.P_W_vmax()
+print('P/W para velocidade máxima: (hp/lb)', P_W_vmax)
+print('P/W para velocidade máxima: (W/kg)', P_W_vmax*745.7/0.453592)
+#estimativa P/W para cruzeiro:
+T_Wcruise = aircraft.T_Wcruise()
+P_Wcruise = aircraft.conversionP_W(T_Wcruise)
+P_Wtakeoff = aircraft.conversionCruiseTakeoff(P_Wcruise)
+print('P/W: (hp/lb)', P_Wtakeoff)
+print('P/W: (W/kg)', P_Wtakeoff*745.7/0.453592)
+    
