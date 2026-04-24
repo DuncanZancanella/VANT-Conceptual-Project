c8cc2cf68b8cc1a5691a26b72f97607f489a31c8
CamiSteinn <camilaehlertm@gmail.com>
2026-04-24 19:54:38 -0300

    adiciona

--- src/Entrega_3/MTOW_Missao1_Reconhecimento.m
+++ src/Entrega_3/MTOW_Missao1_Reconhecimento.m
@@ -30,9 +30,9 @@ h_s = 2000; % teto de serviço
 
 v = Vc_kmh/3.6   ; % Velocidade (m/s)
 T = 275.1            ; % Temperatura do ar (K)
-a = sqrt(1.4*T*287)  ;
-M = v/a              ;
-
+a_m = sqrt(1.4*T*287)  ;
+M = v/a_m              ;
+vmax = 300/1.863;
 
 
 % -- Estimativa da aeronave:
@@ -40,7 +40,9 @@ Swet_Sref = 3.5;
 LD_max = 18;
 LD_c = LD_max;
 LD_l = 0.866 * LD_max;
-
+AR = 14;
+P_W = 0.073;
+W_S = 14.4;
 C_c_hr = 0.4; %consumo específico cruzeiro
 C_c_s = C_c_hr/3600;
 
@@ -54,10 +56,20 @@ c = -0.16;
 % -- Peso Payload
 Wpl = CP_lb;
 
+% -- Peso Motor
+Wmotor = 164.2444;
+
 % -- Peso Vazio
-Kvs = 1
-%WeW0 = %@(W0)  A*(W0^c) * Kvs;
-We = 631.95 % valor código missão 2
+a = -0.25;
+b = 1.18;
+C1 = -0.20;
+C2 = 0.08;
+C3 = 0.05;
+C4 = -0.05;
+C5 = 0.27;
+
+%WeW0 = @(W0) a + b*(W0^C1)* (AR^C2)*(P_W^C3)*(W_S^C4)*(vmax^C5) ;
+We =  1918.4 % valor código missão 2
 % -- Peso Combustível
 
 % W1/W0
@@ -81,8 +93,9 @@ W6W5 = 0.995
 WfW0 = 1.06*(1 - W1W0*W2W1*W3W2*W4W3*W5W4*W6W5);
 
 % --- Resolvendo W0
-R = @(W0) W0 - (Wpl + WfW0*W0 + We) ;
-W0 = fzero(R, 200)
+%R = @(W0) W0 - (Wpl + Wmotor + WfW0*W0 + WeW0(W0)*W0 ) ;
+R = @(W0) W0 - (Wpl + Wmotor + WfW0*W0 + We) ;
+W0 = fzero(R, 1000)
 
 Wf = WfW0*W0
 
@@ -94,7 +107,8 @@ W3 = W3W2*W2
 W4 = W4W3*W3
 W5 = W5W4*W4
 W6 = W6W5*W5
-Wf/W0
+WfW0 = Wf/W0
+%We = WeW0(W0)*W0
 
 x = [0,1,2,3,4,5,6];
 plot(x,[W0,W1,W2,W3,W4,W5,W6], 'b--o',  'LineWidth', 3)
